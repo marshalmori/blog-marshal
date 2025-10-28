1ec6b3b1703838bedccdba3212686f07f39e7f23
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2025-10-28 11:33:21 -0300

    Modifying the canonical URL to SEO-friendly

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     publish = models.DateTimeField(default=timezone.now)
@@ -36,6 +36,14 @@ class Post(models.Model):
 
 
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.id])
+        return reverse(
+            "blog:post_detail",
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+                ]
+            )
 
 
